362b875ae7e71cb6175b9c02dce3a21f663f718d
ShunedZun <109823801+ShunedZun@users.noreply.github.com>
2026-04-29 21:51:30 -0400

    Update code.s

--- code.s
+++ code.s
@@ -1,21 +1,76 @@
 .section .data
-outtext: .asciz "The double is: "
-numtext: .asciz
+outtext: .asciz "\nThe double is: "
+numtext: .asciz "%ld"
 outleng = . - outtext
 
 .section .bss
-number: .space 4
+numberin: .space 32
+numberout: .space 32
 
 .section .text
 .globl _start
 
 _start:
-mov $0, %eax
-mov $0, %edi
-mov $number, %esi
-mov $4, %edx
+
+#get the input
+mov $3, %eax
+mov $0, %ebx
+mov $numberin, %ecx
+mov $32, %edx
+int $0x80
+
+mov $numberin, %esi
+xor %eax, %eax
+
+#convert to an integer
+convert:
+movzbl (%esi), %ebx
+cmp $10, %ebx
+je done
+
+sub $'0', %ebx
+
+imul $10, $eax, $eax
+add %ebx, %eax
+
+inc %esi
+jmp convert
+
+#point to now double the value
+done:
+add %eax, %eax
+move $numout, %esi
+add $31, %esi
+
+movb $0, (%esi)
+mov $10, %ebx
+
+#convert into string
+sconvert:
+dec %esi
+xor %edx, %edx
+div $ebx
+
+add $'0', %dl
+move %dl, (%esi)
+
+jnz sconvert
+
+#output
+mov $4, %eax
+mov $1, %ebx
+mov $outtext, %ecx
+move $outleng, %edx
 int $0x80
 
 mov $4, %eax
 mov $1, %ebx
-mov $outtext, %ecx
+mov %esi, %ecx
+mov $numout, %edx
+add $31, %edx
+sub %esi, %edx
+int $0x80
+
+mov $1, %eax
+xor %ebx, %ebx
+int $0x80
